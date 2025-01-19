2328b7fc2ca8ecf6dfd0dbe1425e2d6746175c6a
Danilo Agostinho <daniloagostinhosilva@gmmail.com>
2025-01-19 15:05:58 -0300

    up

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-root',
+	imports: [],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss',
+})
+export class AppComponent {
+	sayHello() {
+		return this.hello();
+	}
+
+	hello() {
+		return 'Hello, World';
+	}
+}
---
+++ src/app/index.js
@@ -0,0 +1,3 @@
+console.log(a); // undefined (elevada, mas não inicializada)
+var a = 10;
+console.log(a); // 10
